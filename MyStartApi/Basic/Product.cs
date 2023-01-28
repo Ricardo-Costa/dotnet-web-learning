0a7c8a1efe081bdbf2f9a61148eb28f1be1a6602
Ricardo Costa <ricardo18acs@gmail.com>
2023-01-28 11:38:10 -0300

    feat: get and set

--- MyStartApi/Basic/Product.cs
+++ MyStartApi/Basic/Product.cs
@@ -2,11 +2,17 @@ namespace MyStartApi.Basic
 {
   public class Product
   {
+    // TODO essa é uma forma antiga de se fazer
     private string _name = null!;
     public string Name
     {
       get
       {
+        if (string.IsNullOrEmpty(_name)) {
+          System.Console.WriteLine("Deu ruim...");
+          throw new Exception("Error empty name property...");
+        }
+        
         return _name;
       }
       set
@@ -15,6 +21,7 @@ namespace MyStartApi.Basic
       }
     }
 
+    // TODO essa é a forma mais atual de se fazer
     public string Description { get; set; } = null!;
     public DateTime  CreatedAt { get; set; }
   }
