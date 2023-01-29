db6e979116612defdc16f3b956686d053285f51a
Ricardo Costa <ricardo18acs@gmail.com>
2023-01-29 17:36:21 -0300

    feat: basic about methods

--- Course/Program.cs
+++ Course/Program.cs
@@ -39,57 +39,62 @@
       idade = null; 
 
 
-    // TODO valores padrões - builtin types sempre possuem um valor padrao
-    /*
-    int => 0
-    float => 0
-    decimal => 0
-    bool => false
-    char => '\0'
-    string => ""
-    */
+      // TODO valores padrões - builtin types sempre possuem um valor padrao
+      /*
+      int => 0
+      float => 0
+      decimal => 0
+      bool => false
+      char => '\0'
+      string => ""
+      */
 
-    // TODO conversao implícitas
-    float valor = 25.4F;
-    int outro = 25;
-    valor = outro; // conversão implicita => Um TIPO ACEITA(É COMPATÍVEL) com o outro.
-    // TODO conversao EXplicita
-    // obrigatoriamente temos que informar o tipo que estamos convertendo.
-    int inteiro = 100;
-    uint interiroSemSinal = (uint)inteiro;
-    inteiro = (int)valor;
-    Console.WriteLine(inteiro);
+      // TODO conversao implícitas
+      float valor = 25.4F;
+      int outro = 25;
+      valor = outro; // conversão implicita => Um TIPO ACEITA(É COMPATÍVEL) com o outro.
+      // TODO conversao EXplicita
+      // obrigatoriamente temos que informar o tipo que estamos convertendo.
+      int inteiro = 100;
+      uint interiroSemSinal = (uint)inteiro;
+      inteiro = (int)valor;
+      Console.WriteLine(inteiro);
 
-    try
-    {
-      // TODO Parse, tenta realizar a conversao
-      int meuInteiro = int.Parse("100"); 
-      // meuInteiro = int.Parse(23.5f.ToString()); ERROR
-      Console.WriteLine(meuInteiro); 
-    }
-    catch (System.Exception)
-    {
-      
-      throw;
-    }
+      try
+      {
+        // TODO Parse, tenta realizar a conversao
+        int meuInteiro = int.Parse("100"); 
+        // meuInteiro = int.Parse(23.5f.ToString()); ERROR
+        Console.WriteLine(meuInteiro); 
+      }
+      catch (System.Exception)
+      {
+        
+        throw;
+      }
 
-    try
-    {
-      int meuInteiro2 = Convert.ToInt32("335");
-      Console.WriteLine(meuInteiro2);
-      meuInteiro2 = Convert.ToInt32(33.5);
-      Console.WriteLine(meuInteiro2);
-    }
-    catch (System.Exception e)
-    {
-      Console.WriteLine(e.Message);
-      // throw;
-    }
+      try
+      {
+        int meuInteiro2 = Convert.ToInt32("335");
+        Console.WriteLine(meuInteiro2);
+        meuInteiro2 = Convert.ToInt32(33.5);
+        Console.WriteLine(meuInteiro2);
+      }
+      catch (System.Exception e)
+      {
+        Console.WriteLine(e.Message);
+        // throw;
+      }
 
-
+      Console.WriteLine(TesteMethod("Ricardo"));
 
       var texto = "Test start app console...";
       Console.WriteLine(texto);
+    }
+
+    static string TesteMethod(string name, int idade = 29) // idade opcional, tem valor default
+    {
+      return $"Nome: {name} tem idade default {idade}";
     }
 
   }
