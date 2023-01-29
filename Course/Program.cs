5de163a5579d488c19fec61455489eeda2732655
Ricardo Costa <ricardo18acs@gmail.com>
2023-01-29 15:57:13 -0300

    feat: builtin type part 1

--- Course/Program.cs
+++ Course/Program.cs
@@ -4,7 +4,40 @@
   {
     public static void Main(string[] args)
     {
-      Console.WriteLine("Test start app console...");
+      // TODO built-in types ou tipos primitivos
+
+      byte meuByte = 125;
+
+      const int year = 2023;
+      // TODO ERROR var month; // ERROR
+      int day;
+
+      // TODO numeros reais
+      float calculo = 2.5f; // menor quantidade de alocação na capacidade
+      double salario = 200_000.3;
+      decimal compra = 300_000_000.456m; // maior
+      compra = 300_000_000.456M; // m ou M => mesma coisa
+
+      // TODO boolean
+      bool verdade = true;
+
+      // TODO char
+      char letra = 'A';
+      // TODO string
+      string meuTexto = "Este é um texto";
+      
+      // TODO object - quando nao sabemos qual tipo irá vir, perdemos um pouco de intelisence
+      // TODO object - se comporta como um ANy do typescript
+      object quantidade = 1;
+      quantidade = 1.7;
+
+      // TODO nullable type, um tipo que pode ser nulo tambem
+      
+
+
+
+      var texto = "Test start app console...";
+      Console.WriteLine(texto);
       Console.WriteLine("Depois debug...");
     }
 
