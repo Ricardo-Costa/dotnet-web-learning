1b89aa17f4a160c65163a054eea6be6dd5392fe1
Ricardo Costa <ricardo18acs@gmail.com>
2023-03-19 10:59:58 -0300

    feat: init sample with date

---
+++ MyAppDate/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyAppDate
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var data = new DateTime();
+      Console.WriteLine(data);
+    }
+  }
+}
