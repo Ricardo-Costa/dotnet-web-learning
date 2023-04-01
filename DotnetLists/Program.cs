8f3a7a57c671a31cebe259a4ad2c77b39263ff0d
Ricardo Costa <ricardo18acs@gmail.com>
2023-04-01 12:19:16 -0300

    feat: basic of array

--- DotnetLists/Program.cs
+++ DotnetLists/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DotnetLists
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+
+      var meuArray = new int[5]; // array de 5 posições
+      meuArray[0] = 12;
+
+      Console.WriteLine("-------------------[Array]----------------------");
+      Console.WriteLine(meuArray[0]);
+
+      Console.WriteLine("-------------------[Array inicializado]----------------------");
+      var meuArray2 = new int[5]{ 12, 13, 14, 15, -12 }; // array de 5 posições
+      Console.WriteLine(meuArray2[3]);
+
+      Console.WriteLine("-------------------[Array varios tipos + structures]----------------------");
+      var meuArray3 = new Teste[1]{ new Teste() }; // array de 5 posições
+      Console.WriteLine(meuArray3[0].Id);
+    }
+  }
+
+  struct Teste
+  {
+    public int Id { get; set; }
+  }
+}
