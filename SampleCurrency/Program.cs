dd7aebeefe4dbda51f28d962e2db6a2e37b8f508
Ricardo Costa <ricardo18acs@gmail.com>
2023-04-01 11:46:47 -0300

    feat: Math formatations

--- SampleCurrency/Program.cs
+++ SampleCurrency/Program.cs
@@ -35,6 +35,25 @@ namespace SampleCurrency
           CultureInfo.CreateSpecificCulture("pt-BR")
         )
       );
+      Console.WriteLine(
+        valor.ToString(
+          "N", // currency --> moeda
+          CultureInfo.CreateSpecificCulture("pt-BR")
+        )
+      );
+
+      Console.WriteLine("-------------------[Arredondamento de valor com Math]----------------------");
+      valor = 10536.25m;
+
+      Console.WriteLine(
+        Math.Round(valor) // arredondamento
+      );
+      Console.WriteLine(
+        Math.Ceiling(valor) // arredondamento para cima
+      );
+      Console.WriteLine(
+        Math.Floor(valor) // arredondamento para baixo
+      );
     }
   }
 }
