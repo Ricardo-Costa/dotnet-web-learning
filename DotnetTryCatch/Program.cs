93f08dba392594866c68f1d815381fe509707dc9
Ricardo Costa <ricardo18acs@gmail.com>
2023-04-01 12:47:38 -0300

    feat: exceptions

--- DotnetTryCatch/Program.cs
+++ DotnetTryCatch/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DotnetTryCatch
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("-----------------[ Tratamento de Exceptions ]-------------------");
+      int[] arrayA = new int[2];
+
+      try
+      {
+        Console.WriteLine(arrayA[3]);
+      }
+      catch
+      {
+        Console.WriteLine("Deu ruim...");
+        // throw;
+      }
+
+      try
+      {
+        Console.WriteLine(arrayA[3]);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("Deu ruim...");
+        Console.WriteLine(ex.Message);
+        // throw;
+      }
+    }
+  }
+}
