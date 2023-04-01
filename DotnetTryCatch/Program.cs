4ec9e6a55c9f039314bf4ef377d04402254fa53f
Ricardo Costa <ricardo18acs@gmail.com>
2023-04-01 12:51:22 -0300

    feat: multi catchs

--- DotnetTryCatch/Program.cs
+++ DotnetTryCatch/Program.cs
@@ -29,6 +29,25 @@ namespace DotnetTryCatch
         Console.WriteLine(ex.Message);
         // throw;
       }
+
+      Console.WriteLine("-----------------[ Tratamento de Exceptions Multiplas ]-------------------");
+
+      try
+      {
+        Console.WriteLine(arrayA[3]);
+      }
+      catch (IndexOutOfRangeException ex)
+      {
+        Console.WriteLine("Deu ruim no indice...");
+        Console.WriteLine(ex.Message);
+        // throw;
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("Deu ruim GERAL...");
+        Console.WriteLine(ex.Message);
+        // throw;
+      }
     }
   }
 }
